3bb6903e4ca12a0b3a47ff195731b8759a44f6ca
Joern Bernhardt <joern.bernhardt@compose.us>
2022-05-19 23:50:50 +0200

    chore: Add map tiles
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- apps/webapp/src/lib/map/CreateMap.svelte
+++ apps/webapp/src/lib/map/CreateMap.svelte
@@ -1,5 +1,9 @@
 <script lang="ts" context="module">
 	let id = 0;
+	const tileProvider = 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
+	const tileProviderAttribution = {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	};
 </script>
 
 <script lang="ts">
@@ -21,6 +25,7 @@
 
 	function createMap(mapOptions?: L.MapOptions) {
 		map = Leaflet.map(mapId, mapOptions);
+		Leaflet.tileLayer(tileProvider, tileProviderAttribution).addTo(map);
 	}
 </script>
 
