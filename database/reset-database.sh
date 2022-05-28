e22a47b5b8bf23b030ae350370559c09bd19cb39
Joern Bernhardt <joern.bernhardt@compose.us>
2022-05-29 01:18:50 +0200

    fix: Add features table to db reset
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- database/reset-database.sh
+++ database/reset-database.sh
@@ -2,7 +2,7 @@
 
 if [[ "${RESET_DB}" = "true" ]]; then
   echo "Resetting database"
-  PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /app/deleteTables.sql -f /app/users.sql
+  PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /app/deleteTables.sql -f /app/users.sql -f /app/features.sql
 else
   echo "No need to reset database (RESET_DB is not set to 'true')"
 fi
