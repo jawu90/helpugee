708f330ac925a6debead7c4f4b2ac67e1d89b1b6
Joern Bernhardt <joern.bernhardt@compose.us>
2022-06-09 22:54:29 +0200

    feat: Load feature on update route
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- apps/webapp/src/routes/map/update/[id].svelte
+++ apps/webapp/src/routes/map/update/[id].svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { API_BASE } from '$lib/core/api';
+	import type { Feature } from '$lib/map/filterable/types';
+	import { useQuery } from '@sveltestack/svelte-query';
 
 	const id = $page.params.id;
+
+	async function getFeature() {
+		const res = await fetch(`${API_BASE}/feature`);
+		const jsonResponse = await res.json();
+		if (!res.ok) {
+			throw new Error(jsonResponse.error_msg);
+		}
+		return jsonResponse as Feature;
+	}
+
+	const featureQuery = useQuery(['feature', { id }], getFeature);
+	$: featureQuery.setOptions(['feature', { id }], getFeature);
 </script>
 
 <svelte:head>
@@ -9,8 +24,13 @@
 	<meta name="description" content="Helpful map for new people" />
 </svelte:head>
 
-<h2>Update an entry</h2>
-<p>Entry {id}</p>
+{#if $featureQuery.isLoading}
+	<h2>Update an entry</h2>
+	<p>Loading entry {id}</p>
+{:else if $featureQuery.data}
+	<h2>{$featureQuery.data.label}</h2>
+	<p>{$featureQuery.data.data.address}</p>
+{/if}
 
 <style>
 </style>
